38f5bf5293fd76f95223866786cecc1938b456cc
suman7490 <suman.dhangar.135497@gmail.com>
2025-01-03 14:03:05 +0530

    added something

--- fetch.js
+++ fetch.js
@@ -6,4 +6,39 @@ async function handlePromise() {
     console.table(apiData)
 
 }
-handlePromise(); 
+// handlePromise(); 
+
+
+function greet(greeting){
+    console.log(`${greeting}, ${this.name}`)
+}
+const user = {name: "Suman"}
+
+greet.call(user, "Hi")
+greet.apply(user, ["Hello"])
+const bindGreet = greet.bind(user, "Hey")
+bindGreet()
+
+
+// ***************** setTimeOut and setInterval *******************
+
+setTimeout(() => {
+    console.log("Hello Guys!")
+},1000)
+const interValid = setInterval(() => {
+console.log("Repeated")
+}, 1000)
+
+clearInterval(interValid);
+
+// ******************************
+const obj = {a: 1, b: { c: 2 }}
+const shallow = {...obj}
+console.log("Object before shallow copy:", shallow)
+shallow.b.c = 3
+console.log("Object after shallow copy:", shallow)
+
+const deep = JSON.parse(JSON.stringify(obj))
+console.log("Object before deep copy:", deep)
+deep.b.c = 4
+console.log("Object after deep copy:", deep)
